f92f0c606af9058360bcf1cc4b06d8011c90968d
Andrukevich <andrukevich.ev@gmail.com>
2018-07-02 10:10:18 +0300

    third lesson

--- js/main.js
+++ js/main.js
@@ -3,10 +3,25 @@ new Vue({
     data : {
         title : "Hello World!",
         styleCSS : "",
+        value : 1,
+        // doubleValue : 1,
     },
     methods : {
         changeText () {
             this.title = "someting new text";
         },
+        increment (value) {
+            this.value = value;
+            // this.doubleValue = value * 2;
+            // this.doubleValue = this.value * 2;
+            if (value == 25) {
+                alert("Number equal 25");
+            }
+        }
     },
+    computed : {
+        doubleValue () {
+            return this.value * 2;
+        }
+    }
 });
