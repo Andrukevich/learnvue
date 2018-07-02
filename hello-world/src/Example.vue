fc00a563911f7860e8f78421016cc99455bdac63
Andrukevich <andrukevich.ev@gmail.com>
2018-07-02 17:06:22 +0300

    eighth lesson

---
+++ hello-world/src/Example.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="example">
+        <h1>Start text!</h1>
+        <app-input></app-input>
+    </div>
+</template>
+
+<script>
+    import Input from './components/Field.vue'
+
+    export default {
+        components: {
+            "app-input": Input
+        }
+    }
+</script>
